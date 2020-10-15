2c244d4374072cd9ad9cb23cef8f530e1d98b411
Lucas Quintanilha <l.quitan@gmail.com>
2020-10-15 00:27:32 -0300

    Update solution

--- Examples/BracoSimples.c
+++ Examples/BracoSimples.c
@@ -3,6 +3,25 @@
 
 static int shoulder = 0, elbow = 0, index = 0, middle = 0, thumb = 0, base = 0;
 
+void init(void);
+void display(void);
+void reshape(int w, int h);
+void keyboard(unsigned char key, int x, int y);
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+	return 0;
+}
+
 void init(void)
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -80,8 +99,7 @@ void display(void)
 	glutSwapBuffers();
 }
 
-void reshape(int w, int h)
-{
+void reshape(int w, int h) {
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -91,8 +109,7 @@ void reshape(int w, int h)
 	glTranslatef(0.0, 0.0, -5.0);
 }
 
-void keyboard(unsigned char key, int x, int y)
-{
+void keyboard(unsigned char key, int x, int y) {
 	switch (key)
 	{
 	case 's':
@@ -149,19 +166,4 @@ void keyboard(unsigned char key, int x, int y)
 	default:
 		break;
 	}
-}
-
-int main(int argc, char **argv)
-{
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow(argv[0]);
-	init();
-	glutDisplayFunc(display);
-	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
-	glutMainLoop();
-	return 0;
 }
